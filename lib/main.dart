6e55fd49c3599b1aedbeb2533a0497c16de746d4
HMZKA <hmzakh65@gmail.com>
2022-10-08 15:44:21 +0300

    column-expanded

--- lib/main.dart
+++ lib/main.dart
@@ -11,33 +11,71 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('First Project'),
-          titleSpacing: 50,
-          actions: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.phone)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.video_call))
-          ],
-        ),
-        body: Container(
-          padding: EdgeInsets.all(20),
-          margin: EdgeInsets.all(50),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50),
-            color: Colors.black,
+          appBar: AppBar(
+            title: Text('First Project'),
+            titleSpacing: 50,
+            actions: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.phone)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.video_call))
+            ],
           ),
-          height: double.infinity,
-          width: double.infinity,
-          child: Text(
-            'Hello World',
-            style: TextStyle(
-                color: Colors.red,
-                fontSize: 30,
-                fontWeight: FontWeight.w700,
-                backgroundColor: Colors.amber),
-          ),
-        ),
-      ),
+          body: Container(
+            color: Colors.amber,
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    color: Colors.black,
+                    child: Text(
+                      'First Text',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.grey,
+                    child: Text(
+                      'Second Text',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.yellow,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.black,
+                    child: Text(
+                      'Third Text',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.pink,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    height: 130,
+                    color: Colors.red,
+                    child: Text(
+                      'Fourth Text',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
